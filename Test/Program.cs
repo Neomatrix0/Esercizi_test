50302c3dc7a7de66a7198245746b550fad0f74ba
Neomatrix0 <danimusci@yahoo.it>
2024-06-20 19:34:11 +0200

    first commit

---
+++ Test/Program.cs
@@ -0,0 +1,32 @@
+﻿//initialization of the variable to the current year
+int currentYear = DateTime.Now.Year;
+
+//ask to type your name
+Console.WriteLine("Hi there,please type your name:");
+
+// read input name
+string? name = Console.ReadLine();
+
+// ask to type your age 
+
+Console.WriteLine("please type your age:");
+
+// convert string input in integer input
+
+int age =  Convert.ToInt32(Console.ReadLine());
+
+// formula to get date of birth as test
+int datebirth = currentYear - age;
+
+// print name, age and birthday
+
+Console.WriteLine($"Hi {name},You are {age} years old.You were born in {datebirth}");
+
+
+
+
+
+
+
+
+
